ac7c895d874f87c23de093750fb8386740b88727
NIcat <nijatshh@code.edu.az>
2023-05-04 16:56:49 +0400

    hometask

---
+++ style.js
@@ -0,0 +1,14 @@
+let arr =[20,15,300,150,25]
+
+
+
+
+function dot(item){
+    console.log(item*2)
+}
+arr.map(dot)
+
+
+// console.log(arr.sort((a,b) => a-b))
+//  console.log(arr[arr.length-1]);
+
